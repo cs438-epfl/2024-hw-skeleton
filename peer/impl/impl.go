be9d890f089640e222506a80d284a40e4eed9d6e
ltran <loris.tran@epfl.ch>
2024-09-15 01:10:51 +0200

    feat: added first edition of start and stop functions, needs testing

--- peer/impl/impl.go
+++ peer/impl/impl.go
@@ -1,8 +1,14 @@
 package impl
 
 import (
+	"errors"
+	"log"
+	"sync"
+	"time"
+
 	"go.dedis.ch/cs438/peer"
 	"go.dedis.ch/cs438/transport"
+	"go.dedis.ch/cs438/types"
 )
 
 // NewPeer creates a new peer. You can change the content and location of this
@@ -19,17 +25,89 @@ func NewPeer(conf peer.Configuration) peer.Peer {
 type node struct {
 	peer.Peer
 	// You probably want to keep the peer.Configuration on this struct:
-	//conf peer.Configuration
+	conf     peer.Configuration
+	socket   transport.ClosableSocket
+	stopChan chan struct{}
+	wg       sync.WaitGroup
 }
 
 // Start implements peer.Service
 func (n *node) Start() error {
-	panic("to be implemented in HW0")
+	n.stopChan = make(chan struct{})
+	n.wg.Add(1)
+
+	go func() {
+		defer n.wg.Done()
+		for {
+			select {
+			case <-n.stopChan:
+				return
+			default:
+				pkt, err := n.conf.Socket.Recv(time.Second * 1)
+				if errors.Is(err, transport.TimeoutError(0)) {
+					continue
+				}
+				if err != nil {
+					// Handle error (log it, for example)
+					log.Printf("Error receiving packet: %v", err)
+					continue
+				}
+
+				// Process the packet
+				err = n.processPacket(pkt)
+				if err != nil {
+					// Log the error
+					log.Printf("Error processing packet: %v", err)
+				}
+			}
+
+		}
+	}()
+
+	return nil
+}
+
+func (n *node) processPacket(pkt transport.Packet) error {
+	// First, unmarshal the message
+	var msg types.Message
+	// Define a constant for the timeout duration
+	const sendTimeout = 5 * time.Second // You can adjust this value as needed
+
+	err := n.conf.MessageRegistry.UnmarshalMessage(pkt.Msg, msg)
+	if err != nil {
+		return err
+	}
+
+	if pkt.Header.Destination == n.conf.Socket.GetAddress() {
+		// The packet is for this node
+		err = n.conf.MessageRegistry.ProcessPacket(pkt)
+		if err != nil {
+			return err
+		}
+	} else {
+		// Packet needs to be relayed
+		pkt.Header.RelayedBy = n.conf.Socket.GetAddress()
+		err := n.conf.Socket.Send(pkt.Header.Destination, pkt, sendTimeout)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 // Stop implements peer.Service
 func (n *node) Stop() error {
-	panic("to be implemented in HW0")
+	// Signal the main loop to stop
+	close(n.stopChan)
+
+	// Wait for the goroutine to finish
+	n.wg.Wait()
+
+	// We can't close the socket directly, so we'll just log that we're stopping
+	log.Println("Node stopped")
+
+	return nil
 }
 
 // Unicast implements peer.Messaging
